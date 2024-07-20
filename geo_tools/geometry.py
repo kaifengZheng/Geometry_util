9f42ad21427b63d8cee2a0f69cf324e74bf7b125
Kaifeng Zheng <kaifeng.zheng@stonybrook.edu>
2024-07-20 14:40:03 -0400

    update

--- geo_tools/geometry.py
+++ geo_tools/geometry.py
@@ -100,7 +100,6 @@ def surface_per(atoms:Atoms,method='CN'):
         CNs,diss,CN_ave=getCN_dis_N(atoms,1,option='CN')
     if method=='GCN':
         CNs,diss,CN_ave=getCN_dis_N(atoms,1,option='GCN')
-    CNs,diss,CN_ave=getCN_dis_N(atoms,1)
     CN_surface=[cn for cn in CNs if cn<np.max(CNs)]
     return len(CN_surface)/len(CNs)
 
